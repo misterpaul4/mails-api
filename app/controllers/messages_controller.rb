256502f73fa5cd6d6dbb7d81f3d3ce68e47d80de
Paul Ajuizeogu <ajuzepaul@gmail.com>
2022-01-24 21:35:13 +0200

    Add controller actions for User and Message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,40 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: %i[show update destroy]
+
+   # POST /messages
+   def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      render json: @message, status: :created, location: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  # PATCH/PUT /messages/1
+  def update
+    if @message.update(message_params)
+      render json: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+    # DELETE /messages/1
+    def destroy
+      @message.destroy
+    end
+
+    private
+
+    def set_message
+      @message = Message.find(params[:id])
+    end
+  
+    # Only allow a list of trusted parameters through.
+    def message_params
+      params.permit(:subject, :content, :flag)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /users/username
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/username
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by_username!(params[:_username])
+    rescue ActiveRecord::RecordNotFound
+      render json: { errors: 'User not found' }, status: :not_found
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.permit(:username)
+  end
 end
