32d69b7fff096468c4382903518f9908c0a92428
Paul Ajuizeogu <ajuzepaul@gmail.com>
2022-01-24 20:57:34 +0200

    Add model associations and validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  # association
+  belongs_to :user
+
+  # validation
+  validates_presence_of :subject
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # association
+  has_many :messages, dependent: :destroy
+
+  # validation
+  validates_presence_of :username
 end
