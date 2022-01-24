92564f4beacc4a70fd15a3f5185f9ea030b25ff0
Paul Ajuizeogu <ajuzepaul@gmail.com>
2022-01-24 21:04:53 +0200

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, param: :_username, only: %i[destroy update show]
+  resources :messages
 end
